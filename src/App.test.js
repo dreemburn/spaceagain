5585bb3a17f160377c9d8618c9369769a9dfa54a
dreemburn <130921578+dreemburn@users.noreply.github.com>
2023-09-28 13:53:47 -0400

    Add files via upload

---
+++ src/App.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders footer', () => {
+  render(<App />);
+  const footerText = screen.getByText(/the new home/i);
+  expect(footerText).toBeInTheDocument();
+});
